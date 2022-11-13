c87498e0391acf4e0cbcf4cf45e63db8f68d710c
Anesu326 <31014178+Anesu326@users.noreply.github.com>
2022-11-13 14:55:09 +0200

    Update Roster_App.js

--- Roster_App.js
+++ Roster_App.js
@@ -2,11 +2,13 @@ var roster = [];
 
 //function to add new student to the roster
 function add_new(name){
+  var name = prompt('Enter name to add to the roster:');
   roster.push(name);
 }
 
 //function to remove a name from the roster
 function remove_item(name){
+  var name = prompt('Enter name to remove from the roster:');
   const index = roster.indexOf(name);
   roster.splice(index, 1);
 }
@@ -19,21 +21,20 @@ function display(){
 //Aplication Logic
 var response = prompt('Do you want to use the Duty Roster App?');
 
-if (response == 'y'){
+if (response == 'yes'){
   while (true) {
     var option = prompt('Select one of the following options\n1. Add Name\n2. Remove Name\n3. Display Roster\n4. Quit App');
-    if (option == 1){
-      var name = prompt('Enter name to add to the roster:');
+    if (option == 1){      
       add_new(name);
       continue;
-    }else if (option == 2){
-      var name = prompt('Enter name to remove from the roster:');
+    }else if (option == 2){      
       remove_item(name);
       continue;
     }else if (option == 3){
       display();
       continue;
     }else if (option == 4){
+      alert('Thank you for using the Roster App!')
       break;
     }else{
       alert('Invalid Input!');
